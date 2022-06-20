099b2ff7a6f8c54092809696e8a0586505cf18b2
lukaradojevic <radojevic2210@gmail.com>
2022-06-20 11:32:44 +0200

    getInfo funkcija kojom se dobijaju informacije o pozicijama F1 vozača na osnovu unete takmičarske godine

--- src/strane/VozaciHome.js
+++ src/strane/VozaciHome.js
@@ -4,10 +4,29 @@ import {useState} from 'react'
 function VozaciHome(){
 
     const [godina, setGodina] = useState('');
+    const [info, setInfo] = useState([]);
 
     function postaviGodinu(e){
         setGodina(e.target.value);
     }
+
+    function getInfo(){
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '4916172c11msh0beee19c8846ca4p127267jsn0bc7bcd7fc43',
+                'X-RapidAPI-Host': 'f1-live-motorsport-data.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://f1-live-motorsport-data.p.rapidapi.com/drivers/standings/'+ godina, options)
+            .then(response => response.json())
+            .then(response => setInfo(response.results))
+            .catch(err => console.error(err));
+
+    }
+
 
     return (
         <div>
@@ -16,7 +35,7 @@ function VozaciHome(){
 
             <h2 id="nas-takm-godine">Unesite takmičarsku godinu</h2>  
             <input type="text" className="form-control" id="unos-takm-godine" onChange={postaviGodinu} value={godina} />  
-            <button type="button" className="btn btn-info btn-lg" id="btnprikazi">Prikaži</button>
+            <button type="button" onClick={getInfo} className="btn btn-info btn-lg" id="btnprikazi">Prikaži</button>
 
         </div>
     )

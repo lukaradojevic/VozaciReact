38a7513619f1afd035a33ee6d7666b73c39298dc
lukaradojevic <radojevic2210@gmail.com>
2022-06-20 11:17:44 +0200

    Implementiran izgled stranice VozaciHome, useState() React Hook

--- src/strane/VozaciHome.js
+++ src/strane/VozaciHome.js
@@ -1,10 +1,22 @@
 import NavBar from "../komponente/NavBar";
+import {useState} from 'react'
 
 function VozaciHome(){
+
+    const [godina, setGodina] = useState('');
+
+    function postaviGodinu(e){
+        setGodina(e.target.value);
+    }
+
     return (
         <div>
-            
+
             <NavBar />  
+
+            <h2 id="nas-takm-godine">Unesite takmičarsku godinu</h2>  
+            <input type="text" className="form-control" id="unos-takm-godine" onChange={postaviGodinu} value={godina} />  
+            <button type="button" className="btn btn-info btn-lg" id="btnprikazi">Prikaži</button>
 
         </div>
     )
